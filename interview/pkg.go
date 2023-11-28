33aaf1dd3c07332f8bb8ddcad858354c7c3e2a23
Shakezidin <sinuzidin@gmail.com>
2023-11-28 18:29:59 +0530

    webhook link added

--- interview/pkg.go
+++ interview/pkg.go
@@ -1,7 +1,10 @@
 package interview
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
+	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -13,7 +16,7 @@ type chs struct {
 	trait string
 }
 
-func InterviewTask(ctx *gin.Context, ch chan map[string]interface{}) {
+func InterviewTask(ctx *gin.Context, ch chan map[string]interface{}, webhookURL string) {
 	attributes := make([]chs, 10)
 	traits := make([]chs, 10)
 	m := map[string]string{
@@ -34,75 +37,84 @@ func InterviewTask(ctx *gin.Context, ch chan map[string]interface{}) {
 		log.Println("unable to marshal")
 		return
 	}
-	result := map[string]interface{}{
-		"attributes": "",
-		"traits":     "",
-	}
-	var i int
+	result := map[string]interface{}{}
+
 	for k, v := range mp {
 		if len(k) >= 4 && k[:4] == "atrk" {
-			i, _ = strconv.Atoi(k[4:])
+			i, _ := strconv.Atoi(k[4:])
 			attributes[i].key = v
 			continue
 		}
 		if len(k) >= 4 && k[:4] == "atrv" {
-			i, _ = strconv.Atoi(k[4:])
+			i, _ := strconv.Atoi(k[4:])
 			attributes[i].value = v
 			continue
 		}
 		if len(k) >= 4 && k[:4] == "atrt" {
-			i, _ = strconv.Atoi(k[4:])
+			i, _ := strconv.Atoi(k[4:])
 			attributes[i].trait = v
 			continue
 		}
 		if len(k) >= 5 && k[:5] == "uatrk" {
-			i, _ = strconv.Atoi(k[5:])
+			i, _ := strconv.Atoi(k[5:])
 			traits[i].key = v
 			continue
 		}
 		if len(k) >= 5 && k[:5] == "uatrv" {
-			i, _ = strconv.Atoi(k[5:])
+			i, _ := strconv.Atoi(k[5:])
 			traits[i].value = v
 			continue
 
 		}
 		if len(k) >= 5 && k[:5] == "uatrt" {
-			i, _ = strconv.Atoi(k[5:])
+			i, _ := strconv.Atoi(k[5:])
 			traits[i].trait = v
 			continue
 		}
 		result[m[k]] = v
-
 	}
 
-	var l []map[string]interface{}
-
+	// Populate attributes in the desired format
+	attributeMap := make(map[string]map[string]interface{})
 	for _, k := range attributes {
 		if k.value == "" {
 			continue
 		}
-		l = append(l, map[string]interface{}{
-			k.key: map[string]string{
-				"value": k.value,
-				"type":  k.trait,
-			},
-		})
+		attributeMap[k.key] = map[string]interface{}{
+			"value": k.value,
+			"type":  k.trait,
+		}
 	}
 
-	var l2 []map[string]interface{}
-
+	// Populate traits in the desired format
+	traitMap := make(map[string]map[string]interface{})
 	for _, k := range traits {
 		if k.value == "" {
 			continue
 		}
-		l2 = append(l2, map[string]interface{}{
-			k.key: map[string]string{
-				"value": k.value,
-				"type":  k.trait,
-			},
-		})
+		traitMap[k.key] = map[string]interface{}{
+			"value": k.value,
+			"type":  k.trait,
+		}
 	}
-	result["attributes"] = l
-	result["traits"] = l2
+
+	result["attributes"] = attributeMap
+	result["traits"] = traitMap
+
+	jsonData, err := json.Marshal(result)
+	if err != nil {
+		log.Println("Error marshaling result to JSON:", err)
+		return
+	}
+
+	// Make an HTTP POST request to the webhook
+	resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Println("Error making POST request to webhook:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	log.Println("Webhook response Status:", resp.Status)
 	ch <- result
 }
--- main.go
+++ main.go
@@ -8,21 +8,23 @@ import (
 	"github.com/shakezidin/interview"
 )
 
+var webhookURL = "https://webhook.site/5412ebdf-ed92-4e67-827c-97967ea85186"
+
 func main() {
 	engine := gin.Default()
 
 	engine.POST("/test", Calling)
 
 	engine.Run(":8080")
-
 }
 
 func Calling(c *gin.Context) {
 	ch := make(chan map[string]interface{})
-	go interview.InterviewTask(c, ch)
-	log.Println("waiting for responseS")
+	go interview.InterviewTask(c, ch, webhookURL)
+	log.Println("waiting for responses")
 	ans := <-ch
 	c.JSON(http.StatusAccepted, gin.H{
 		"data": ans,
 	})
 }
+
